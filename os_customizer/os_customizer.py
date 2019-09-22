3e161a41208c73f325b9737a5760dde3faec2109
Chris <chris.hodder@beeksfx.com>
2019-09-22 13:33:07 +0100

    WIP: Working on Windows Customization, and the OSCustomizer classes.

--- os_customizer/os_customizer.py
+++ os_customizer/os_customizer.py
@@ -4,53 +4,104 @@ from os_customizer.centos_customizer import CentOSCustomizer
 from os_customizer.windows_customizer import WindowsCustomizer
 
 
+class VMCustomizationSpec:
+    """
+    Data class for holding settings which will be applied to the VM being customized.
+    """
+
+    def __init__(self,
+                 admin_username,
+                 admin_password,
+                 ip,
+                 network_device_mac,
+                 netmask="255.255.255.0",
+                 default_gateway=None):
+        """
+        :param str admin_username: Username of the administrator account the script can log in as
+        :param str admin_password: Password for the adminsitrator account
+        :param str ip: IP to set on the box
+        :param str network_device_mac: MAC address of the network device we're going to configure
+        :param str netmask: Netmask to set on that device (defaults to 255.255.255.0)
+        :param str default_gateway: (optional) defaults to .1 on the subnet of the given IP. (only works for ipv4)
+        """
+        self.admin_username = admin_username
+        self.admin_password = admin_password
+        self.ip = ip
+        self.network_device_mac = network_device_mac
+        self.netmask = netmask
+
+        if default_gateway:
+            self.default_gateway = default_gateway
+        else:
+            # Default to .1 in the last octet of the IP
+            self.default_gateway = ip.rpartition(".")[0] + '.1'
+
+
 class OSCustomizer(ABC):
     """
     Base class with builder, defines interfaces for the implementation classes.
     """
 
+    command_wait_seconds = 5  # time to wait between running commands.
+
     @staticmethod
-    def os_customizer_builder(os_type, vsphere, vm_name, admin_account_details):
+    def os_customizer_builder(os_type, vsphere, vm_name, spec):
         """
         Get an appropriate OS customizer instance for the given VM
 
         :param str os_type: Windows/CentOS
         :param vsphere vsphere:
         :param str vm_name:
-        :param dict admin_account_details: Should contain 'username' and 'password' keys for logging in to the OS
+        :param VMCustomizationSpec spec:
         :return:
         """
 
         if os_type.lower() == "windows":
-            return WindowsCustomizer(vsphere, vm_name, admin_account_details)
+            return WindowsCustomizer(vsphere, vm_name, spec)
 
         if os_type.lower() == 'centos':
-            return CentOSCustomizer(vsphere, vm_name, admin_account_details)
+            return CentOSCustomizer(vsphere, vm_name, spec)
 
         return NotImplementedError(f"No suitable customizer for OS type {os_type}")
 
-    def __init__(self, vsphere, vm_name, admin_account_details):
+    def __init__(self, vsphere, vm_name, spec):
         """
         Set up class variables and get an interface for talking to the guest OS
 
         :param VSphere vsphere:
         :param str vm_name:
-        :param dict admin_account_details:
+        :param VMCustomizationSpec spec: settings used for customizing this VM
         """
         self.vsphere = vsphere
         self.vm_name = vm_name
-        self.admin_username = admin_account_details.get('username')
-        self.admin_password = admin_account_details.get('password')
+        self.spec = spec
 
         self.os_interface = vsphere.get_guestosinterface_for_vm(self.vsphere,
                                                                 self.vm_name,
-                                                                self.admin_username,
-                                                                self.admin_password)
+                                                                self.spec.admin_username,
+                                                                self.spec.admin_password)
+        self._populate_command_list()
+
+    def _populate_command_list(self):
+        """
+        Stub. Extended by child lists to define which commands will be run on the OS.
+
+        :return:
+        """
         self.command_list = []
 
     def customize(self):
-        # stub, implemented by child classes
-        raise NotImplementedError()
+        """
+        Run commands (defined by the command_list) on the guest OS of the VM (specified in init)
+
+        TODO: Maybe refactor round to specify the VM here?
+        :return:
+        """
+        try:
+            self._process_commands()
+        except Exception as e:
+            print(e)
+            # TODO - proper handling
 
     def _process_commands(self):
         """
--- os_customizer/windows_customizer.py
+++ os_customizer/windows_customizer.py
@@ -10,11 +10,26 @@ class WindowsCustomizer(OSCustomizer):
     properly.
     """
 
-    def customize(self):
+    def define_command_list(self):
 
+        # Locations of common programs you'll want to run in Windows
         powershell_path = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
         cmd_path = "C:\\Windows\\System32\\cmd.exe"
         cscript_path = "C:\\Windows\\System32\\cscript.exe"
+
+        # Name for default network. Used because it is significantly easier to change the network settings if
+        # you know the network adapter /name/ and it's significantly more reliable to simply set it to a known value
+        # than try to dump the name somewhere then pull it back to this script.
+        # Warning: I've seen some weird issues with the adapter name. It must not contain spaces or >1 capital letter
+        # Windows shouldn't care, and won't if you do it directly on the box, but it doesn't like it from the script...
+        new_net_adapter_name = "virtnet"
+
+        # Windows has a funny idea of MAC addresses
+        windows_network_device_mac = self.config.network_device_mac.upper().replace(":", "-")
+
+        '''
+            Command defs
+        '''
 
         rename_computer = {
             'command': GuestOSCommand(
@@ -25,7 +40,109 @@ class WindowsCustomizer(OSCustomizer):
             'success_required': False,
         }
 
+        # Note I've experienced issue with line breaks in commands sent to windows, so I keep them on one line
+
+        disable_terminal_server_rdp_tcp_auth = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command="/c REG ADD \"HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" /v UserAuthentication /t REG_DWORD /d 0 /f",
+                description='Disable Terminal Server RDP-TCP user auth',
+                output_file_location='C:\\set_rdp_auth.log',
+                success_outputs=['The operation completed successfully.']
+            ),
+            'success_required': False,  # May already be disabled
+        }
+
+        start_terminal_service_on_boot = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command="/c sc config TermService start=auto",
+                description='Start the terminal service automatically on Windows start',
+                output_file_location='C:\\term_service_auto_start.log',
+                success_outputs=['[SC] ChangeServiceConfig SUCCESS']
+            ),
+            'success_required': False,  # May already be enabled
+        }
+
+        enable_terminal_service_encryption = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command="/c REG ADD \"HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" /v MinEncryptionLevel /t REG_DWORD /d 1 /f",
+                description='Enable terminal server encryption',
+                output_file_location='C:\\set_term_serv_encryption.log',
+                success_outputs=['The operation completed successfully.']
+            ),
+            'success_required': False,  # May already be enabled
+        }
+
+        set_net_adapter_name = {
+            'command': GuestOSCommand(
+                program_path=powershell_path,
+                program_command=f"-c \"Get-NetAdapter | where {{$_.MacAddress -eq \\\"{windows_network_device_mac}\\\"}} | rename-NetAdapter -NewName {new_net_adapter_name}\"",
+                description=f'Set the NetAdapter name to {new_net_adapter_name}',
+                output_file_location='C:\\set_net_adapater_name.log',
+                success_outputs=['']
+            ),
+            'success_required': True  # May already be enabled
+        }
+
+        set_up_networking = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command="/c netsh interface ip set address name=\"{}\" static {} {} {}".formtat(
+                    new_net_adapter_name,
+                    self.spec.ip,
+                    self.spec.netmask,
+                    self.spec.default_gateway
+                ),
+                description=f'Set up networking on {new_net_adapter_name}',
+                output_file_location='C:\\network_settings.log',
+                success_outputs=['']
+            ),
+            'success_required': True
+        }
+
+        set_primary_dns = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command=f"/c netsh interface ip set dns name=\"{new_net_adapter_name}\" static 1.1.1.1 primary validate=no",
+                description=f'Set primary DNS',
+                output_file_location='C:\\dns_1.log',
+                success_outputs=['']
+            ),
+            'success_required': True
+        }
+
+        set_secondary_dns = {
+            'command': GuestOSCommand(
+                program_path=cmd_path,
+                program_command=f"/c netsh interface ip set dns name=\"{new_net_adapter_name}\" static 9.9.9.9 index=2 validate=no",
+                description=f'Set secondary DNS',
+                output_file_location='C:\\dns_2.log',
+                success_outputs=['The object is already in the list.']
+            ),
+            'success_required': False
+        }
+
+
+
 
         first_command_list = [
             rename_computer,
+            disable_terminal_server_rdp_tcp_auth,
+            start_terminal_service_on_boot,
+            enable_terminal_service_encryption,
+        ]
+
+        # Need to reboot before doing networking, specifically Windows 2012 tends to screw up otherwise
+        second_command_list = [
+            set_net_adapter_name,
+            set_up_networking,
+            set_primary_dns,
+            set_secondary_dns
+        ]
+
+        self.command_list = [
+            first_command_list,
+            second_command_list,
         ]
